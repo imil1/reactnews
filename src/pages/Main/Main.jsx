2afabc27b1de0904a8db79fde11211b8502d16ef
imil <imil.net@mail.ru>
2024-07-11 22:45:35 +0500

    add pagination change api

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,23 +4,44 @@ import { getNews } from '../../api/apiNews';
 import NewsBanner from '../../components/newsBanner/NewsBanner';
 import NewsList from '../../components/NewsList/NewsList';
 import Skeleton from '../../components/skeleton/Skeleton';
+import Pagination from '../../components/Pagination/Pagination';
 
 const Main = () => {
     const [news, setNews] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
+    const totalPages = 10;
+    const pageSize = 10;
+
+    const fetchNews = async (currentPage) => {
+        try {
+            setIsLoading(true);
+            const res = await getNews(currentPage, pageSize);
+            setNews(res.news);
+            setIsLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     useEffect(() => {
-        const fetchNews = async () => {
-            try {
-                setIsLoading(true);
-                const res = await getNews();
-                setNews(res.news);
-                setIsLoading(false);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        fetchNews();
-    }, []);
+        fetchNews(currentPage);
+    }, [currentPage]);
+
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+    const handlePrevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+    const handlePageClick = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
     return (
         <main className={style.main}>
             {news.length > 0 && !isLoading ? (
@@ -31,6 +52,13 @@ const Main = () => {
                     type="banner"
                 />
             )}
+            <Pagination
+                totalPages={totalPages}
+                handleNextPage={handleNextPage}
+                handlePageClick={handlePageClick}
+                handlePrevPage={handlePrevPage}
+                currentPage={currentPage}
+            />
             {!isLoading ? (
                 <NewsList news={news} />
             ) : (
@@ -39,6 +67,13 @@ const Main = () => {
                     type="item"
                 />
             )}
+            <Pagination
+                totalPages={totalPages}
+                handleNextPage={handleNextPage}
+                handlePageClick={handlePageClick}
+                handlePrevPage={handlePrevPage}
+                currentPage={currentPage}
+            />
         </main>
     );
 };
