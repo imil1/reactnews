a51d7c3b5715d20bc1f26f1d68a3399995fee4ef
imil <imil.net@mail.ru>
2024-07-11 19:27:10 +0500

    skeleton to dev

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -13,7 +13,7 @@ const Main = () => {
             try {
                 setIsLoading(true);
                 const res = await getNews();
-                 setNews(res.news);
+                setNews(res.news);
                 setIsLoading(false);
             } catch (error) {
                 console.log(error);
@@ -35,7 +35,7 @@ const Main = () => {
                 <NewsList news={news} />
             ) : (
                 <Skeleton
-                    count={10}
+                    count={11}
                     type="item"
                 />
             )}
