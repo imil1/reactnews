0d40d5f4b1b01356658befbe8e44ef6076ebb65b
imil <imil.net@mail.ru>
2024-07-20 22:51:44 +0500

    need merge

---
+++ src/components/LatestNews/LatestNews.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './latestnews.module.scss'
+
+const LatestNews = () => {
+  return (
+    <section className={style.section}>LatestNews</section>
+  )
+}
+
+export default LatestNews
